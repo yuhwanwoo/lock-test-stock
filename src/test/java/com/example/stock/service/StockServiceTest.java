cf59f19ea8bb405a287079a08b9fdcbf58d581cb
yuhwanwoo <6514687@naver.com>
2024-01-21 11:28:09 +0900

    [lock] 레이스 컨디션 발생

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,73 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void 재고감소() {
+        stockService.decrease(1L, 1L);
+
+        // 100 - 1 = 99
+        Stock stock = stockRepository.findById(1L)
+                .orElseThrow();
+
+        assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 스레드에서 수행중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        Stock stock = stockRepository.findById(1L)
+                .orElseThrow();
+        // 100 - (1 * 100) = 0 예상
+        // 레이스 컨디션이 발생해 실패한다.
+        assertEquals(0, stock.getQuantity());
+    }
+}
