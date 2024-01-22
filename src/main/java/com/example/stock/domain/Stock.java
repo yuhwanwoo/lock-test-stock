9564097e52cfaa97991f285b9231d7bfd6998839
yuhwanwoo <6514687@naver.com>
2024-01-22 14:46:35 +0900

    [lock] 낙관적 락 정리

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Version;
 
 @Entity
 public class Stock {
@@ -13,6 +14,9 @@ public class Stock {
 
     private Long productId;
     private Long quantity;
+
+    @Version
+    private Long version;
 
     public Stock() {
     }
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -48,32 +48,31 @@ class StockServiceTest {
         assertEquals(99, stock.getQuantity());
     }
 
-//    @Test
-//    void 동시에_100개의_요청() throws InterruptedException {
-//        int threadCount = 100;
-//        ExecutorService executorService = Executors.newFixedThreadPool(32);
-//
-//        // 다른 스레드에서 수행중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
-//        CountDownLatch latch = new CountDownLatch(threadCount);
-//
-//        for (int i = 0; i < threadCount; i++) {
-//            executorService.submit(() -> {
-//                try {
-//                    stockService.decrease(1L, 1L);
-//                } finally {
-//                    latch.countDown();
-//                }
-//            });
-//        }
-//
-//        latch.await();
-//        Stock stock = stockRepository.findById(1L)
-//                .orElseThrow();
-//        System.out.println("stock.getId() = " + stock.getId());
-//        // 100 - (1 * 100) = 0 예상
-//        // 레이스 컨디션이 발생해 실패한다.
-//        assertEquals(0, stock.getQuantity());
-//    }
+    @Test
+    void 동시에_100개의_요청() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        // 다른 스레드에서 수행중인 작업이 완료될 때까지 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        Stock stock = stockRepository.findById(1L)
+                .orElseThrow();
+        // 100 - (1 * 100) = 0 예상
+        // 레이스 컨디션이 발생해 실패한다.
+        assertEquals(0, stock.getQuantity());
+    }
 
     @Test
     void 재고감소_with_pessimistic() {
